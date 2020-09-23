a2c4161df89ad97ea42a29d6cc35b84a55215f12
Rafael Nogueira Rabelo <rafaelnrabelo@gmail.com>
2020-09-23 13:15:22 -0300

    Update Folders Schema

---
+++ Backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -0,0 +1,10 @@
+import Appointment from "../infra/typeorm/entities/Appointment";
+
+import ICreateAppointmentDTO from "../dtos/ICreateAppointmentDTO";
+
+interface IAppointmentsRepository {
+  create(data: ICreateAppointmentDTO): Promise<Appointment>;
+  findByDate(date: Date): Promise<Appointment | undefined>;
+}
+
+export default IAppointmentsRepository;
