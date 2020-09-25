e7b42d2b9aa5f828da87106bd57318e680fb95f7
Rafael Nogueira Rabelo <rafaelnrabelo@gmail.com>
2020-09-25 16:53:40 -0300

    Add Tests

---
+++ Backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,34 @@
+import { v4 as uuid } from "uuid";
+import { isEqual } from "date-fns";
+
+import IAppointmentsRepository from "@modules/appointments/repositories/IAppointmentsRepository";
+import ICreateAppointmentDTO from "@modules/appointments/dtos/ICreateAppointmentDTO";
+
+import Appointment from "@modules/appointments/infra/typeorm/entities/Appointment";
+
+class AppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, { id: uuid(), date, provider_id });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find((appointment) =>
+      isEqual(appointment.date, date)
+    );
+
+    return findAppointment;
+  }
+}
+
+export default AppointmentsRepository;
